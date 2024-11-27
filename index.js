d30707daa592744eed897fb1e127affde1a14cfe
HannaOdud <159914920+HannaOdud@users.noreply.github.com>
2024-11-27 13:19:59 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ let images, current_image;
 images = ['images/random1.jfif','images/random2.jfif' ,'images/random3.jfif']
 let element_image = document.getElementById('carousel-image');
 element_image.setAttribute("src", images[0]);
+current_image = images[0];
 
 
 document.getElementById('next').addEventListener('click', (event) => {
-  current_image = images.shift();
-  images.push(current_image);
+  images.push(images.shift());
+  current_image = images[0];
   let element_image2 = document.getElementById('carousel-image');
-  element_image2.setAttribute("src", images[0]);
+  element_image2.setAttribute("src", current_image);
 });
 
 
 document.getElementById('previous').addEventListener('click', (event) => {
-  current_image = images.pop();
-  images.unshift(current_image);
+  images.unshift(images.pop());
+  current_image = images[0];
   let element_image3 = document.getElementById('carousel-image');
-  element_image3.setAttribute("src", images.slice(-1)[0]);
+  element_image3.setAttribute("src", current_image);
 });
